1beceed84b417718502a15a204c15562907212c9
jramma <jrmasip097@gmail.com>
2022-10-17 13:37:41 +0200

    latitud

--- UsAcompanyem-Backend/src/main/java/org/hackathon/grup3/app/model/Barrio.java
+++ UsAcompanyem-Backend/src/main/java/org/hackathon/grup3/app/model/Barrio.java
@@ -1,6 +1,13 @@
 package org.hackathon.grup3.app.model;
 
+import com.google.gson.Gson;
 import lombok.*;
+import org.hackathon.grup3.app.model.coords.BarriCoords;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Getter
 @Setter
@@ -16,6 +23,20 @@ public class Barrio {
     private double lat;
     private double lon;
 
+
+    private void addCords( ){
+        HttpRequest getCoordsRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://w33.bcn.cat/geoBCN/serveis/territori/barris/01"))
+                .GET()
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpResponse<String> postResponse = httpClient.send(getCoordsRequest, HttpResponse.BodyHandlers.ofString());
+
+        Gson gson = new Gson();
+        BarriCoords barriCoords = gson.fromJson(postResponse.body(), BarriCoords.class);
+    }
 
 
 
